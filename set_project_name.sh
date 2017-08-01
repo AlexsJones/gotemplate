ff8f2a6e317280a9744297715fc94e548e56dd7d
AlexsJones <tibbar@freedommail.ch>
2017-08-01 09:47:40 +0100

    simple golang template

--- set_project_name.sh
+++ set_project_name.sh
@@ -3,7 +3,7 @@
 #     File Name           :     set_project_name.sh
 #     Created By          :     
 #     Creation Date       :     [2017-08-01 08:57]
-#     Last Modified       :     [2017-08-01 09:01]
+#     Last Modified       :     [2017-08-01 09:43]
 #     Description         :      
 #################################################################################
 mv  project_name $1
