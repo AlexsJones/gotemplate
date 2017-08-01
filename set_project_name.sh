00332b0630f396b52800469b9c8f8813abaf1e0f
AlexsJones <tibbar@freedommail.ch>
2017-08-01 09:01:52 +0100

    updated

--- set_project_name.sh
+++ set_project_name.sh
@@ -3,8 +3,8 @@
 #     File Name           :     set_project_name.sh
 #     Created By          :     
 #     Creation Date       :     [2017-08-01 08:57]
-#     Last Modified       :     [2017-08-01 08:59]
+#     Last Modified       :     [2017-08-01 09:01]
 #     Description         :      
 #################################################################################
-mv -rf project_name $1
-sed -i 's/project_name/$1/g' docker_compose.yml
+mv  project_name $1
+sed -i "s/project_name/$1/g" docker-compose.yml
