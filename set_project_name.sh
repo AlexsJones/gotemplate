cb2f2a32a75138386e2ff744147611a06e38316b
AlexsJones <tibbar@freedommail.ch>
2017-08-01 09:48:11 +0100

    simple golang template

--- set_project_name.sh
+++ set_project_name.sh
@@ -3,8 +3,10 @@
 #     File Name           :     set_project_name.sh
 #     Created By          :     
 #     Creation Date       :     [2017-08-01 08:57]
-#     Last Modified       :     [2017-08-01 09:43]
+#     Last Modified       :     [2017-08-01 09:48]
 #     Description         :      
 #################################################################################
 mv  project_name $1
 sed -i "s/project_name/$1/g" docker-compose.yml
+echo "Deleting .git folder"
+rm -rf .git
